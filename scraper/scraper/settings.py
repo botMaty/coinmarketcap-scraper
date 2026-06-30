695a342da28db224d9b0408e3e9546ced3d3299a
Seyed Mohammad Mahdi Moallem <bot_maty@Seyeds-MacBook-Air.local>
2026-06-30 15:38:58 +0330

    add playwright settings

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -85,3 +85,18 @@ DOWNLOAD_DELAY = 1
 
 # Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
+
+# Playwright settings
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+}
+
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+
+
+# Optional
+PLAYWRIGHT_LAUNCH_OPTIONS = {
+    "headless": False,
+} 
+PLAYWRIGHT_BROWSER_TYPE = "chromium"
