971c3637d8c2f49617e7e405156eaf021f837e96
Seyed Mohammad Mahdi Moallem <bot_maty@Seyeds-MacBook-Air.local>
2026-06-30 16:25:09 +0330

    add search_for_coins spider

---
+++ scraper/scraper/spiders/search_for_coin.py
@@ -0,0 +1,37 @@
+import scrapy
+from scrapy_playwright.page import PageMethod
+from scraper.utilis.all_coins_inf import get_url_by_sym
+
+
+class ScrapingClubSpider(scrapy.Spider):
+    name = "search_for_coin"
+    allowed_domains = ["coinmarketcap.com"]
+
+    start_urls = [
+        f"https://coinmarketcap.com" + get_url_by_sym("TRX")
+    ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, meta={
+                "playwright": True,
+                "playwright_include_page": True,
+                "playwright_page_methods": [
+                    # PageMethod("wait_for_load_state", "networkidle"),
+                    PageMethod("wait_for_selector", 'span[data-test="text-cdp-price-display"]'),
+                ],
+            })
+
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+
+        html = await page.content()
+
+        response = response.replace(body=html)
+
+        
+        yield {
+            "Name": response.css('span[data-role="coin-name"]::text').get(),
+            "Price": response.css('span[data-test="text-cdp-price-display"]::text').get(),
+        }
+        await page.close()
