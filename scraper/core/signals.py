2dc7258bfbdbed13ad740909a13f4bfe002ad05b
Seyed Mohammad Mahdi Moallem <bot_maty@Seyeds-MacBook-Air.local>
2026-07-01 11:41:27 +0330

    add signals

---
+++ scraper/core/signals.py
@@ -0,0 +1,12 @@
+from scrapy import signals
+
+def connect(crawler, collector):
+    crawler.signals.connect(
+        collector.item_scraped,
+        signal=signals.item_scraped,
+    )
+
+    crawler.signals.connect(
+        collector.spider_closed,
+        signal=signals.spider_closed,
+    )
