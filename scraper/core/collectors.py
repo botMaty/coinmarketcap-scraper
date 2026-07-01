e3b2c316156a5458616d998936af70e7aff60990
Seyed Mohammad Mahdi Moallem <bot_maty@Seyeds-MacBook-Air.local>
2026-07-01 11:42:10 +0330

    add list collector

---
+++ scraper/core/collectors.py
@@ -0,0 +1,11 @@
+from scrapy import signals
+
+class ListCollector:
+    def __init__(self):
+        self.items = []
+
+    def item_scraped(self, item, response, spider):
+        self.items.append(dict(item))
+
+    def spider_closed(self, spider, reason):
+        print("Spider finished")
